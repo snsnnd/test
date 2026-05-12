7e6445d957c1ccdf454bd2bc92da1a1b043a6ee4
snsnnd <2152202573@qq.com>
2026-05-12 16:30:14 +0800

    Increase Atlas bridge UART baudrate and buffer

--- WS63/管理/atlas_bridge_config.h
+++ WS63/管理/atlas_bridge_config.h
@@ -6,16 +6,17 @@
 
 /*
  * Bridge UART settings for WS63 MASTER -> Atlas 200I serial uplink.
- * Update these macros before building the JS_MASTER bridge image.
+ * 注意：Atlas 端 main.py 启动时也必须使用相同波特率，例如：
+ * python3 main.py serve --serial-baudrate 921600
  */
 
 #define BRIDGE_UART_BUS                 UART_BUS_2
-#define BRIDGE_UART_BAUDRATE            115200
+#define BRIDGE_UART_BAUDRATE            921600
 #define BRIDGE_UART_TX_PIN              GPIO_07
 #define BRIDGE_UART_RX_PIN              GPIO_08
 #define BRIDGE_UART_TX_PIN_MODE         PIN_MODE_2
 #define BRIDGE_UART_RX_PIN_MODE         PIN_MODE_2
-#define BRIDGE_UART_RX_BUFFER_SIZE      64
-#define BRIDGE_UART_WRITE_TIMEOUT_MS    0
+#define BRIDGE_UART_RX_BUFFER_SIZE      512
+#define BRIDGE_UART_WRITE_TIMEOUT_MS    100
 
 #endif
